710251ca8b99a9829317c7fb0c33e1e82d954c12
David Huanco Acero <87257377+dhuancero@users.noreply.github.com>
2022-08-31 11:38:13 +0200

    New version
    Add new version

--- js/functions.js
+++ js/functions.js
@@ -1,43 +1,70 @@
 const saveContact = (db, contact) => {
-    if (contact.name === '' || contact.number === '' || contact.direction === '') {
-      return showErrorContact()
-    }
-    db.setItem(contact.id, JSON.stringify(contact));
-    window.location.href = '/'
+  if (
+    contact.name === "" ||
+    contact.number === "" ||
+    contact.direction === ""
+  ) {
+    return showErrorContact();
   }
-  
-  const getContact = (db) => {
-  
-    let getContact = Object.keys(db)
-    // console.log(getContact)
-    for (clave of getContact) {
-      const contacto = JSON.parse(db.getItem(clave))
-      createContact(contacto)
-      // console.log(contacto)
-    }
+  db.setItem(contact.id, JSON.stringify(contact));
+  window.location.href = "/";
+};
+
+const getContact = (db) => {
+  let getContact = Object.keys(db);
+  // console.log(getContact)
+  /**
+   * ? Creamos la estructura HTML
+   */
+  const contact = document.querySelector(".list-contacts");
+  const addContact = document.createElement("table");
+  addContact.classList.add("table");
+  addContact.classList.add("table-striped");
+
+  addContact.innerHTML = `
+  <thead>
+    <tr>
+      <th scope="col" class="text-success text-uppercase">Nombre</th>
+      <th scope="col" class="text-success text-uppercase">Teléfono</th>
+      <th scope="col" class="text-success text-uppercase">Direccion</th>
+    </tr>
+  </thead>
+  <tbody class="listContent">
+  </tbody>
+`;
+  contact.appendChild(addContact);
+
+  for (clave of getContact) {
+    const contacto = JSON.parse(db.getItem(clave));
+    createContact(contacto);
+    // console.log(contacto)
   }
-  
-  const createContact = (contacto) => {
-    //*: Mostramos los valores con JS
-    const contact = document.querySelector('.list-contacts')
-    const addContact = document.createElement('div')
-    addContact.classList.add('contact')
-    addContact.innerHTML = `
-      <h3>${contacto.name}</h3>
-      <p>${contacto.number}</p>
-      <p>${contacto.direction}</p>
-      <span class="material-icons icon" onClick="deleteContact('${contacto.id}')">remove_circle</span>
-      `
-    contact.appendChild(addContact)
-  }
-  
-  
-  const deleteContact = (contactId) => {
-    const db = window.localStorage
-    db.removeItem(contactId)
-    window.location.href = '/'
-  }
-  
-  const showErrorContact = () => {
-    alert('Error')
-  }
+};
+
+const createContact = (contacto) => {
+  //*: Mostramos los valores con JS
+  // const contact = document.querySelector(".list-contacts");
+  // const addContact = document.createElement("div");
+  // addContact.classList.add("contact");
+  const newContact = document.querySelector(".listContent");
+  const addNewContact = document.createElement("tr");
+  addNewContact.classList.add("table-active");
+  addNewContact.innerHTML = `
+      <th scope="row">${contacto.name}</th>
+      <td>${contacto.number}</td>
+      <td>${contacto.direction}</td>
+      <td><span class="material-icons text-danger icon" onClick="deleteContact('${contacto.id}')">remove_circle</span></td>
+  `;
+  newContact.appendChild(addNewContact);
+};
+
+const deleteContact = (contactId) => {
+  const db = window.localStorage;
+  db.removeItem(contactId);
+  window.location.href = "/";
+};
+
+const showErrorContact = () => {
+  alert("Error");
+  const message = document.querySelector(".message");
+};
--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 /* Obtenemos las constantes de los datos */
-const nameData = document.querySelector('.nombre')
-const number = document.querySelector('.numero')
-const direction = document.querySelector('.direccion')
-const btnAddContact = document.querySelector('.btn-add-contacts')
+const nameData = document.querySelector(".nombre");
+const number = document.querySelector(".numero");
+const direction = document.querySelector(".direccion");
+const btnAddContact = document.querySelector(".btn-add-contacts");
 
-const listContacts = document.querySelector('.list-contacts')
+const listContacts = document.querySelector(".list-contacts");
 
-const db = window.localStorage
+const db = window.localStorage;
 
 btnAddContact.onclick = () => {
   // almacenalos los valores en un objeto
@@ -14,8 +14,8 @@ btnAddContact.onclick = () => {
     id: Math.random(1, 100),
     name: nameData.value,
     number: number.value,
-    direction: direction.value
-  }
-  saveContact(db, contact)
-}
-getContact(db)
+    direction: direction.value,
+  };
+  saveContact(db, contact);
+};
+getContact(db);
